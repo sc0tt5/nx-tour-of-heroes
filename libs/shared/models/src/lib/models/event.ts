b7ca76afc34c9c66b492ad782d347802a40c9b98
Scott Robertson <contact@ScottRobertson.io>
2020-03-01 00:26:19 -0600

    simplified dataLayer payload/logic

--- libs/shared/models/src/lib/models/event.ts
+++ libs/shared/models/src/lib/models/event.ts
@@ -2,4 +2,6 @@ export interface Event {
   action: string;
   category: string;
   label: string;
+  event?: string;
+  eventCallback?: () => void;
 }
--- libs/shared/utils/src/lib/track-event.ts
+++ libs/shared/utils/src/lib/track-event.ts
@@ -1,35 +1,32 @@
+import { Event } from '@nx-demo/shared/models';
+
 /**
  * GA event tracking via GTM datalayer
  */
-export function trackEvent(action: string, category: string, label: string): void {
+export function trackEvent(
+  action: string,
+  category: string,
+  label: string,
+  callback?: () => void
+): void {
   try {
     let isEventDone = false; // prevent double tracking when applicable
-    const dataLayer = ((<any>window).dataLayer = (<any>window).dataLayer || []); // check if dataLayer loaded, if not then create it
-    const data: Event | any =
-      !!action && !!category && !!label
-        ? {
-            action,
-            category,
-            label,
-            event: 'ngTrackEvent',
-            eventCallback: null
-          }
-        : null;
+    const isValidEvent: boolean = !!action && !!category && !!label;
 
-    // if data hydrated then push
-    if (data) {
-      function callback() {
-        if (!isEventDone) {
-          dataLayer.push(data);
-          isEventDone = true;
-        }
-      }
-
-      if (callback) {
-        data.eventCallback = callback;
-      }
-
+    if (isValidEvent && !isEventDone) {
+      // check if dataLayer loaded, if not then create it
+      const dataLayer: any = ((<any>window).dataLayer = (<any>window).dataLayer || []);
+      // event data
+      const data: Event = {
+        action,
+        category,
+        label,
+        eventCallback: callback || null,
+        event: 'ngTrackEvent'
+      };
+      // push to dataLayer
       dataLayer.push(data);
+      isEventDone = true;
     }
   } catch (error) {
     console.log({ error });
