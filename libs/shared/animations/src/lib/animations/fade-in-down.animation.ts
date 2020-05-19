7b93ca4caa9a52906475fd93908276a717f9cd3d
Scott Robertson <contact@ScottRobertson.io>
2020-05-18 22:48:32 -0500

    animation unit tests (wip)

--- libs/shared/animations/src/lib/animations/fade-in-down.animation.ts
+++ libs/shared/animations/src/lib/animations/fade-in-down.animation.ts
@@ -3,15 +3,10 @@ import { animate, animation, AnimationTriggerMetadata, keyframes, style, transit
 
 const DEFAULT_TIMINGS = '500ms';
 
-const fadeInDown = animation([
-  animate(
-    '{{ timings }}',
-    keyframes([
-      style({ opacity: 0, transform: 'translate3d(0, -{{ translate }}, 0)' }),
-      style({ opacity: 1, transform: 'translate3d(0, 0, 0)' })
-    ])
-  )
-]);
+const startStyle = style({ opacity: 0, transform: 'translate3d(0, -{{ translate }}, 0)' });
+const endStyle = style({ opacity: 1, transform: 'translate3d(0, 0, 0)' });
+
+const fadeInDown = animation([animate('{{ timings }}', keyframes([startStyle, endStyle]))]);
 
 /**
  * Fade-in the element from top-to-bottom by percent
