abb5d44ef2140b74ac21141aaa88a234540821d3
Scott Robertson <contact@ScottRobertson.io>
2022-12-31 00:40:46 -0600

    issue-72 testing LHCI action - token and lh adj

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -7,11 +7,17 @@ module.exports = {
         'categories:accessibility': ['error', { minScore: 1 }],
         'categories:best-practices': ['error', { minScore: 1 }],
         'categories:seo': ['error', { minScore: 1 }],
-        'cumulative-layout-shift': ['warn', { maxNumericValue: 0 }],
+        'cumulative-layout-shift': ['warn', { maxNumericValue: 0.1 }],
         'first-contentful-paint': ['warn', { maxNumericValue: 750 }],
         interactive: ['error', { maxNumericValue: 1000 }],
         'largest-contentful-paint': ['warn', { maxNumericValue: 1200 }],
-        'speed-index': ['warn', { maxNumericValue: 1000 }]
+        'speed-index': ['warn', { maxNumericValue: 1000 }],
+        // application not setup for production server
+        'csp-xss': 'off',
+        'unused-css-rules': 'off', // remove from styles bundle if possible
+        'unused-javascript': 'off', // remove from main bundle if possible
+        'uses-long-cache-ttl': 'off',
+        'uses-text-compression': 'off'
       }
     },
     collect: {
