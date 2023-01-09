b8aa60352f92ca73602f2b096ed17598c50e2214
Scott Robertson <contact@ScottRobertson.io>
2023-01-08 20:59:26 -0600

    issue-42 remove unused components and services

--- libs/shared/ui/src/lib/content-loader/content-loader.module.ts
+++ libs/shared/ui/src/lib/content-loader/content-loader.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { LoadingIndicatorModule } from './../loading-indicator/loading-indicator.module';
 import { ContentLoaderComponent } from './content-loader.component';
 
 @NgModule({
-  imports: [CommonModule, LoadingIndicatorModule],
+  imports: [CommonModule],
   declarations: [ContentLoaderComponent],
   exports: [ContentLoaderComponent]
 })
